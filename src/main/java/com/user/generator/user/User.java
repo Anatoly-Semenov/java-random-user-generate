d5151436144626992704de8cec67f7883416c189
Anatoly Semenov <anatolysrmenov@gmail.com>
2023-07-01 22:03:44 +0300

    Create static user module

---
+++ src/main/java/com/user/generator/user/User.java
@@ -0,0 +1,80 @@
+package com.user.generator.user;
+
+import java.time.LocalDate;
+
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(Long id, String firstName, String lastName, LocalDate dob, Integer age) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public User(String firstName, String lastName, LocalDate dob, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/user/generator/user/UserController.java
@@ -0,0 +1,30 @@
+package com.user.generator.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/users")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return this.userService.getUsers();
+    }
+
+    @GetMapping(path = "{id}")
+    public User getUser(@PathVariable("id") Long id) {
+        return this.userService.getUser(id);
+    }
+}
